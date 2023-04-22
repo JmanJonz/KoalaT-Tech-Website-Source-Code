c3599bd90c4c62feeebcdbcfb2166b85cff9f45e
Jammonjones <wjssaan@gmail.com>
2023-04-22 15:18:52 -0600

    button animation and instructions

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,25 @@ entranceButton.addEventListener("click", ()=>{
     main.style.display = "block";
     footer.style.display = "block";
 });
+
+setTimeout(() => {
+    const entrance_instructions = document.createElement("p");
+    entrance_instructions.textContent = "Click the koala to continue to site.";
+    entranceText = document.querySelector(".entrancetext")
+    entranceText.append(entrance_instructions);
+    entrance_instructions.style.color = "#659dbd";
+    entrance_instructions.style.fontSize = "1.1rem";
+    entrance_instructions.style.marginTop = "6px";
+
+    let timesMoved = 0;
+    function animateImage(){
+        if (timesMoved < 500){
+            entranceButton.style.transform = `translatex(${timesMoved * 5}px)`;
+            timesMoved += 1;
+            setTimeout(() => {
+                requestAnimationFrame(animateImage);
+            }, 1000);
+        }
+    }
+    animateImage();
+}, 2500);
