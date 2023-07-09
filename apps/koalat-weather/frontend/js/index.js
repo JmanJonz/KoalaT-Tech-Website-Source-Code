d2f735c9beb15ec528b31bf7df1066d0c3953a04
Jammonjones <wjssaan@gmail.com>
2023-07-08 21:56:37 -0600

    sdfsdd

--- apps/koalat-weather/frontend/js/index.js
+++ apps/koalat-weather/frontend/js/index.js
@@ -1,1 +1,10 @@
 // JS for index / home view.
+const locationForm = document.querySelector(".index>form");
+const locationInput = document.querySelector(".location");
+// make form submit upon clicking enter in input
+locationInput.addEventListener("keypress", (event)=>{
+    if(event.key === "Enter"){
+        event.preventDefault();
+        locationForm.submit();
+    }
+})
