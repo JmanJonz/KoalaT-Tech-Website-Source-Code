ef2c972bb612dd1b0c0464af274c31384ceac135
Jammonjones <wjssaan@gmail.com>
2023-07-18 17:13:45 -0600

    lskjdf

--- apps/koalat-weather/frontend/js/index.js
+++ apps/koalat-weather/frontend/js/index.js
@@ -1,16 +1,10 @@
 // JS for index / home view.
 const locationForm = document.querySelector(".index>form");
-const locationInput = document.querySelector(".location");
+const locationInput = document.querySelector("#location");
 const weatherRenderContainer = document.querySelector(".weatherRenderContainer");
 const weatherLocationTitle = document.querySelector(".weatherLocationTitle");
+const theWeatherContainer = document.querySelector(".theWeatherContainer");
 
-// make form submit upon clicking enter in input
-// locationInput.addEventListener("keypress", (event)=>{
-//     if(event.key === "Enter"){
-//         event.preventDefault();
-//         locationForm.submit();
-//     }
-// })
 // Grab the location and if valid use it to fetch weather data with api
 const url = new URL(window.location.href);
 const searchParams = url.searchParams;
@@ -26,7 +20,7 @@ function capitalizeFirstLetter(str) {
 const locationn = params["location"];
 // Perform further processing with the URL parameters
 async function getWeatherByLocation(locationn){
-    locationn = locationn.split(' ').join(',');
+    locationn = locationn.split(' ').join(', ');
     const coordinateResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${locationn}&appid=ac915a6d1258935157073b6ba78cb9f4`);
     const coordinatesData = await coordinateResponse.json();
     // Grab the lat and long
@@ -39,9 +33,24 @@ async function getWeatherByLocation(locationn){
     // Get just the data I want to use
 
     // Render in the data to the DOM
-    weatherLocationTitle.textContent = `Showing Weather For: ${capitalizeFirstLetter(locationn)}`;
-    weatherRenderContainer.insertAdjacentHTML = weatherDataString;
-
+    weatherLocationTitle.textContent = `${capitalizeFirstLetter(locationn)} - Weather`;
+    theWeatherContainer.innerHTML = weatherDataString;
 } getWeatherByLocation(locationn);
+
+const originalPlaceholder = locationInput.getAttribute('placeholder');
+
+// Add an event listener for the 'focus' event
+locationInput.addEventListener('focus', function() {
+  // Clear the input's placeholder text on focus
+  locationInput.removeAttribute('placeholder');
+});
+
+// Add an event listener for the 'blur' event
+locationInput.addEventListener('blur', function() {
+  // Restore the original placeholder text if the input is empty
+  if (!locationInput.value.trim()) {
+    locationInput.setAttribute('placeholder', originalPlaceholder);
+  }
+});
 
 
