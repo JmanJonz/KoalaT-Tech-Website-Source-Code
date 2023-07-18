1ae2333dc74e210f8974aad6f6028dbca5207849
Jammonjones <wjssaan@gmail.com>
2023-07-17 21:50:47 -0600

    last visit more than day ->entrance view

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,22 +2,31 @@
 
 // If the user navigates to the website and it has been a while send them to the entrance view, if it hasn't been more than 1 minutes since navigation don't redirect to navigation view
 // Check if entrance page has been visited before
-let hasVisitedEntrance = localStorage.getItem('hasVisitedEntrance');
-if (hasVisitedEntrance == null || hasVisitedEntrance == ""){
-    hasVisitedEntrance = false;
+  // Get the last visit time from localStorage
+  const lastVisitString = localStorage.getItem("lastVisit");
+
+  // Parse the lastVisitString back to a Date object
+  const lastVisit = new Date(lastVisitString);
+
+  // Set the current visit time
+  const now = new Date();
+  localStorage.setItem("lastVisit", now.toJSON());
+
+  // Check if it has been over a day since the last visit
+  const difference = now - lastVisit;
+  const daysSinceLastVisit = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+  // If it has been over a day, do something
+  if (daysSinceLastVisit > 1) {
+    window.location.href = "frontend/views/entrance.html";
+  }else{
+    // Redirect to the homepage if entrance page has been visited before
+    // Do nothing because we are already at the home page
 }
-console.log(hasVisitedEntrance);
-if (!hasVisitedEntrance || hasVisitedEntrance == false) {
-  // Set flag in localStorage to indicate entrance page has been visited
-  window.location.href = "frontend/views/entrance.html";
-} else {
-  // Redirect to the homepage if entrance page has been visited before
-  // Do nothing because we are already at the home page
-}
-// Set flag to false after 30 seconds
-setTimeout(() => {
-    localStorage.removeItem('hasVisitedEntrance');
-  }, 150000); // 60 seconds delay (30000 milliseconds)
+
+
+
+
 
 // Index main homepage functionality
 // open and close navigation
